263dd912ffa8417cb6092013f2119e90e105ba5d
huypham1411 <54835897+huypham1411@users.noreply.github.com>
2020-03-17 19:06:44 +0700

    fix btn

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,12 +5,18 @@ import '../styles/components/Header.css';
 const Header =()=>{
     return (
         <div className="header">
-            <img src={Logo} alt="Logo"/>
-            <div className='header-btn'> 
-            <Button name='Login'/>
-            <Button name='Sign up' color='#FD5E53'/>
+            <div>
+                <img src={Logo} alt="Logo"/>
+            </div>
+            
+            <div id = "btn1">
+                <Button name='Login' className="header-btn"/>
+            </div>
+            <div id = "btn2">
+                <Button name='Sign up' className="header-btn" color='#FD5E53' />
             </div>
         </div>
     );
 }
 export default Header;
+
