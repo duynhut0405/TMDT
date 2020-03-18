ce258f171a0e5d2e07e12eeac2739937bb75c567
huypham1411 <54835897+huypham1411@users.noreply.github.com>
2020-03-18 20:52:41 +0700

    update searchbar

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import '../styles/components/Nav.css';
+
+
 const Nav =()=>{
     return (
         <div className="nav-bar">
-           <ul>
-               <li>Home</li>
-               <li>About us</li>
-               <li>Our product</li>
-           </ul>
-           <input type="search"></input>
+            <div>
+                <ul>
+                    <li>Home</li>
+                    <li>About us</li>
+                    <li>Our product</li>
+                </ul>
+            </div>
+            <div id = "searchBar">                
+                <input type="search" id ="searchInput"></input>
+            </div>
         </div>
     );
 }
