3b1c8bcda58b1af989a42629861c93a8428297ef
Van Vo <vothanhvan522@gmail.com>
2020-03-31 22:12:58 +0700

    Commit đàu tiên

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import Home from './containers/Home';
-import './App.css';
-import Header from './components/General/Header';
-import Nav from './components/General/Nav';
-import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
-import './fonts/Fondamento-Regular.ttf';
+import React from "react";
+import Home from "./containers/Home";
+import "./App.css";
+import Header from "./components/General/Header";
+import Nav from "./components/General/Nav";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./fonts/Fondamento-Regular.ttf";
 
 function App() {
   return (
-  <Router>
-    <div className="App">
-      <Header/>
-      <Nav/>
-      <Switch>
-      <Route path="/" exact component={Home}/>
-      </Switch>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Nav />
+        <Switch>
+          <Route path="/" exact component={Home} />
+        </Switch>
+      </div>
     </Router>
   );
 }
