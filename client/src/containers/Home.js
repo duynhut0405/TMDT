38e1d3fb9a2809dc3552f6ce8e7b3249bc6526a1
Thanh Hiền Phạm <akirayorunoe@gmail.com>
2020-04-01 20:04:47 +0700

    Add:Circle&Hotbar

--- client/src/containers/Home.js
+++ client/src/containers/Home.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import '../styles/containers/Home.css';
 import BgHeader from '../components/Home/BgHeader';
+import CircleWrapper from '../components/Home/CircleWrapper';
+import HotBar from '../components/Home/HotBar';
 const Home =()=>{
     return (
         <div className="home">
             <BgHeader/>
+            <CircleWrapper />
+            <HotBar/>
         </div>
     );
 }
