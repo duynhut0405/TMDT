a1f73e6db37e3b1966337e9fc10d847435fef750
khang pro 123 <zauncorp@gmail.com>
2020-04-04 21:13:05 +0700

    a

--- client/src/components/Policy/Text.js
+++ client/src/components/Policy/Text.js
@@ -5,8 +5,9 @@ const Policy = () => {
     return (
       <div className="Policy">
           <div className="container">
-          <h1>Our Policy</h1>
-          <h2> Privacy Policy for Company Name</h2>
+          <h2>Our Policy</h2>
+          <p> Privacy Policy for Company Name</p>
+
           <p> At Website Name, accessible at Website. com, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by Website Name and how we use it.</p>
             <p>If you have additional questions or require more information about our Privacy Policy, do not hesitate to contact us through email at Email@Website.com</p>
             <p>This privacy policy applies only to our online activities and is valid for visitors to our website with regards to the information that they shared and/or collect in Website Name. This policy is not applicable to any information collected offline or via channels other than this website.</p>
@@ -35,68 +36,31 @@ const Policy = () => {
             <p>Google</p>
             <p>https://policies.google.com/technologies/ads</p>
             <p>Advertising Partners Privacy Policies</p>
+            <p>You may consult this list to find the Privacy Policy for each of the advertising partners of Website Name.</p>
+            <p>Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on Website Name, which are sent directly to users' browser. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit.</p>
+            <p>Note that Website Name has no access to or control over these cookies that are used by third-party advertisers.</p>
+            <p>Third-Party Privacy Policies</p>
+            <p>Website Name's Privacy Policy does not apply to other advertisers or websites. Thus, we are advising you to consult the respective Privacy Policies of these third-party ad servers for more detailed information. It may include their practices and instructions about how to opt-out of certain options. You may find a complete list of these Privacy Policies and their links here: Privacy Policy Links.</p>
+            <p>You can choose to disable cookies through your individual browser options. To know more detailed information about cookie management with specific web browsers, it can be found at the browsers' respective websites. What Are Cookies?</p>
+            <p>CCPA Privacy Policy (Do Not Sell My Personal Information)</p>
+            <p>Under the CCPA, among other rights, California consumers have the right to:</p>
+            <p>Request that a business that collects a consumer's personal data disclose the categories and specific pieces of personal data that a business has collected about consumers.</p>
+            <p>Request that a business delete any personal data about the consumer that a business has collected.</p>
+            <p>Request that a business that sells a consumer's personal data, not sell the consumer's personal data.</p>
+            <p>If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please contact us.</p>
+            <p>GDPR Privacy Policy (Data Protection Rights)</p>
+            <p>We would like to make sure you are fully aware of all of your data protection rights. Every user is entitled to the following:</p>
+            <p>The right to access – You have the right to request copies of your personal data. We may charge you a small fee for this service.</p>
+            <p>The right to rectification – You have the right to request that we correct any information you believe is inaccurate. You also have the right to request that we complete the information you believe is incomplete.</p>
+            <p>The right to erasure – You have the right to request that we erase your personal data, under certain conditions.</p>
+            <p>The right to restrict processing – You have the right to request that we restrict the processing of your personal data, under certain conditions.</p>
+            <p>The right to object to processing – You have the right to object to our processing of your personal data, under certain conditions.</p>
+            <p>The right to data portability – You have the right to request that we transfer the data that we have collected to another organization, or directly to you, under certain conditions.</p>
+            <p>If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please contact us.</p>
+            <p>Children's Information</p>
+            <p>Another part of our priority is adding protection for children while using the internet. We encourage parents and guardians to observe, participate in, and/or monitor and guide their online activity.</p>
+            <p>Website Name does not knowingly collect any Personal Identifiable Information from children under the age of 13. If you think that your child provided this kind of information on our website, we strongly encourage you to contact us immediately and we will do our best efforts to promptly remove such information from our records.</p>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-You may consult this list to find the Privacy Policy for each of the advertising partners of Website Name.
-
-Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on Website Name, which are sent directly to users' browser. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit.
-
-Note that Website Name has no access to or control over these cookies that are used by third-party advertisers.
-
-Third-Party Privacy Policies
-
-Website Name's Privacy Policy does not apply to other advertisers or websites. Thus, we are advising you to consult the respective Privacy Policies of these third-party ad servers for more detailed information. It may include their practices and instructions about how to opt-out of certain options. You may find a complete list of these Privacy Policies and their links here: Privacy Policy Links.
-
-You can choose to disable cookies through your individual browser options. To know more detailed information about cookie management with specific web browsers, it can be found at the browsers' respective websites. What Are Cookies?
-
-CCPA Privacy Policy (Do Not Sell My Personal Information)
-
-Under the CCPA, among other rights, California consumers have the right to:
-
-Request that a business that collects a consumer's personal data disclose the categories and specific pieces of personal data that a business has collected about consumers.
-
-Request that a business delete any personal data about the consumer that a business has collected.
-
-Request that a business that sells a consumer's personal data, not sell the consumer's personal data.
-
-If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please contact us.
-
-GDPR Privacy Policy (Data Protection Rights)
-
-We would like to make sure you are fully aware of all of your data protection rights. Every user is entitled to the following:
-
-The right to access – You have the right to request copies of your personal data. We may charge you a small fee for this service.
-
-The right to rectification – You have the right to request that we correct any information you believe is inaccurate. You also have the right to request that we complete the information you believe is incomplete.
-
-The right to erasure – You have the right to request that we erase your personal data, under certain conditions.
-
-The right to restrict processing – You have the right to request that we restrict the processing of your personal data, under certain conditions.
-
-The right to object to processing – You have the right to object to our processing of your personal data, under certain conditions.
-
-The right to data portability – You have the right to request that we transfer the data that we have collected to another organization, or directly to you, under certain conditions.
-
-If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please contact us.
-
-Children's Information
-
-Another part of our priority is adding protection for children while using the internet. We encourage parents and guardians to observe, participate in, and/or monitor and guide their online activity.
-
-Website Name does not knowingly collect any Personal Identifiable Information from children under the age of 13. If you think that your child provided this kind of information on our website, we strongly encourage you to contact us immediately and we will do our best efforts to promptly remove such information from our records.
 
           </div>
       </div>
