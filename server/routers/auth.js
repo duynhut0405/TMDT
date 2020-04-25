d4a8f9c23d8b8b10f6fc4b26c98cc74cec1cea33
Thanh Hiền Phạm <akirayorunoe@gmail.com>
2020-04-25 11:46:33 +0700

    Fix

--- server/routers/auth.js
+++ server/routers/auth.js
@@ -4,7 +4,7 @@ const {registerValidation,loginValidation} =require('../validation.js');
 var bcrypt = require('bcryptjs');
 
 //Register
-router.post('/SignIn',async (req,res)=>{
+router.post('/SignUp',async (req,res)=>{
    //Validate
    const {error} = registerValidation(req.body);
    if(error) {return res.status(400).send(error.details); }
