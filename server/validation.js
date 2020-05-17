df0b833aa503f7d1167ed59f812a21bae3a8fb2b
huypham1411 <54835897+huypham1411@users.noreply.github.com>
2020-05-17 19:07:12 +0700

    addfix

--- server/validation.js
+++ server/validation.js
@@ -4,7 +4,9 @@ const registerValidation=(data)=>{//req.body
 const schema=Joi.object({
    name:Joi.string().min(2).required(),
    email:Joi.string().min(6).required().email(),
-   password:Joi.string().min(6).required()
+   password:Joi.string().min(6).required(),
+   address:Joi.string().min(10).required(),
+   phonenum:Joi.number().min(7).required()
 })
 //Validate user
 return schema.validate(data);
