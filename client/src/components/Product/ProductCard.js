d31638d189cf56cc43db6bf8c0c06c1df7ae063f
Nguyễn Khương <khuongtt@outlook.com.vn>
2020-05-20 22:32:37 +0700

    add info

--- client/src/components/Product/ProductCard.js
+++ client/src/components/Product/ProductCard.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import '../../styles/components/Product/ProductCard.css';
 // import ProductImg from './ProductImg';
-// import {addToCart} from '../../action/cart-action'
+import { addToCart } from '../../action/cart-action'
 import QuantitiesButton from './QuantitiesButton';
-class ProductCard extends React.Component{
-    render(){
-        return(
+import { connect } from 'react-redux'
+class ProductCard extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            quantity: 1
+        }
+    }
+    handleClickAdd = (id) => {
+        this.props.addToCart(id, this.state.quantity)
+    }
+    changeQuantity=(quantity)=>{
+        this.setState({quantity})
+    }
+    render() {
+        return (
             <div className="cardProduct">
-                <div className = "detail">
+                <div className="detail">
                     <div>
                         <img className="productInfoImg" src={this.props.img} alt={this.props.productName} />
                     </div>
@@ -19,15 +32,17 @@ class ProductCard extends React.Component{
                         <div>
                             <h3>Price: {this.props.price} $</h3>
                         </div>
-                        <div className = "numberOfProducts">
+                        <div className="numberOfProducts">
                             <div>
-                                <QuantitiesButton/>     
+                                <QuantitiesButton quantity={this.state.quantity} changeQuantity={this.changeQuantity}/>
                             </div>
-                            <button title="add" className="addBtn">ADD</button>
+                            <button title="add" className="addBtn" onClick={() => {
+                                this.handleClickAdd(this.props.id)
+                            }}>ADD</button>
                         </div>
                     </div>
                 </div>
-                <div className = "description">
+                <div className="description">
                     <h3>Description</h3>
                     <p>{this.props.description}</p>
                 </div>
@@ -35,5 +50,15 @@ class ProductCard extends React.Component{
         )
     }
 }
+const mapStateToProps = (state) => {
+    return {
+        items: state.cartReducer
+    }
+}
+const mapDispatchToProps = (dispatch) => {
 
-export default ProductCard;
+    return {
+        addToCart: (id, quantity) => { dispatch(addToCart(id, quantity)) }
+    }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(ProductCard);
